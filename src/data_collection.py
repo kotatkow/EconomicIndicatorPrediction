57e3bc271dd140e84572f059827acf1c4a1ede80
kotatkow <ghkjs0810@gmail.com>
2024-11-07 02:19:54 +0900

    add new function for fetching multiple data series

--- src/data_collection.py
+++ src/data_collection.py
@@ -5,3 +5,16 @@ def fetch_data_from_fred(series_id, api_key):
     fred = Fred(api_key=api_key)
     data = fred.get_series(series_id)
     return pd.DataFrame(data, columns=[series_id])
+
+def fetch_multiple_series(series_ids, api_key):
+    fred = Fred(api_key=api_key)
+    data_frames = []
+    
+    for name, series_id in series_ids.items():
+        series_data = fred.get_series(series_id)
+        df = pd.DataFrame(series_data, columns=[name])
+        data_frames.append(df)
+    
+    # Combine all data frames on the date index
+    combined_data = pd.concat(data_frames, axis=1)
+    return combined_data
